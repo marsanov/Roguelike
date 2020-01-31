8c55a535425692ce84a71cb002a5353cdf1da426
gleb.marsanov <gleb.marsanov@gmail.com>
2020-01-31 10:55:12 +0300

    Главное меню

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] string sceneToLoad;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
